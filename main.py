9cbca7af59c204074a295ce0d9cc763b43498ff2
DiSiCo00 <147243174+DiSiCo00@users.noreply.github.com>
2023-10-22 00:58:47 +0200

    Update main.py

--- main.py
+++ main.py
@@ -144,9 +144,9 @@ if page_selected == "Flamero":
                 # Cloumna de Datos
                 info_col.markdown(f"<h2>{room_type_id_pointer}:</h2>", unsafe_allow_html=True)
                 info_col.divider()
-                info_col.markdown(f"<h3>Precio de la estancia:</h3> €{round(obj['P_Alojamiento'], 2)}", unsafe_allow_html=True)
+                info_col.markdown(f"<h3>Precio de la estancia:</h3> €{round(obj['Precio alojamiento'], 2)}", unsafe_allow_html=True)
                 info_col.markdown(f"<h3>Probabilidad de Cancelación:</h3> {round(cancel_prob*100, 2)}%", unsafe_allow_html=True)
-                info_col.markdown(f"<h3>Pago adelantado:</h3> €{round(cuota*obj['P_Alojamiento'] , 2)}", unsafe_allow_html=True)
+                info_col.markdown(f"<h3>Pago adelantado:</h3> €{round(cuota*obj['Precio alojamiento'] , 2)}", unsafe_allow_html=True)
                 
                 info_col.markdown(f"<h3>Cancelación Gratuita Hasta:</h3> {c_date}", unsafe_allow_html=True)
                 info_col.markdown(f"<h3>Cancel_Score:</h3> {round(score, 2)}", unsafe_allow_html=True)
